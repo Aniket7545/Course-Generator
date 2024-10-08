af553137a135184b1bfe8f1547d99cc484351461
Aniket7545 <aniket.verma2022@vitstudent.ac.in>
2024-10-08 13:01:58 +0530

    Changed the sign-in page

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -30,12 +30,11 @@ export default function Page() {
           </a>
   
           <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-            Welcome to Squid 🦑
+            Welcome to CourseForge AI 🦑
           </h2>
   
           <p className="mt-4 leading-relaxed text-white/90">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+            Design your courses as you want and ace the learning process.
           </p>
         </div>
       </section>
