645a1afd68852d80367ade9475c5088a227635e0
Aniket7545 <aniket.verma2022@vitstudent.ac.in>
2024-10-22 15:18:59 +0530

    Removed AWS part

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    images: {
-        unoptimized: true,
-        domains: [
-            'firebasestorage.googleapis.com',
-            'courseforgebucket.s3.ap-south-1.amazonaws.com'
-        ]
+    images:{
+        domains:['firebasestorage.googleapis.com']
     }
 };
 
